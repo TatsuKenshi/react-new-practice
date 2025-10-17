06486091528f8eb8770ae6e6622ec90f07483b98
Tatsukenshi <rockinbg@gmail.com>
2025-10-17 19:04:58 +0200

    lessons 43 - 52 fundamentals complete

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,54 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import img1 from "./images/book-1.jpg";
-import img2 from "./images/book-2.jpg";
-import img3 from "./images/book-3.jpg";
-
-const booksList = [
-  {
-    image: img1,
-    title: "Interesting Facts For Curious Minds",
-    author: "Jordan Moore",
-    id: 1,
-  },
-  {
-    image: img2,
-    title: "Atomic Habits",
-    author: "James Clear",
-    id: 2,
-  },
-  {
-    image: img3,
-    title: "Fairy Tale",
-    author: "Stephen King",
-    id: 3,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const Booklist = () => {
   return (
-    <section className="booklist">
-      {booksList.map((book) => {
-        return <Book {...book} key={book.id} />;
-      })}
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const { image, title, author, children } = props;
-  const displayTitle = () => {
-    console.log(title);
-  };
-  return (
-    <article className="book">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={displayTitle}>display title</button>
-      <h4>{author}</h4>
-      {children}
-    </article>
+    <>
+      <section>
+        <h1>Amazon Bestsellers</h1>
+      </section>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
