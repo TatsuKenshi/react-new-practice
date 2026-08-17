d4b4f6fc1836805e08c6f1aa53a06145ce8586a0
TatsuKenshi <rockinbg@gmail.com>
2026-08-17 21:19:56 +0200

    30-52

---
+++ 01-fundamentals/src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./img/book-1.jpg";
+import img2 from "./img/book-2.jpg";
+import img3 from "./img/book-3.jpg";
+
+export const books = [
+  {
+    author: "Matt Dinniman",
+    title: "Dungeon Crawler Carl",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "William Golding",
+    title: "Lord of the Flies",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Harper Lee",
+    title: "To Kill a Mockingbird",
+    img: img3,
+    id: 3,
+  },
+];
--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,28 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  );
-};
-
-const author = "Lyla Jadeborn";
-
-const Book = () => {
-  const title = "The Secret Bookshop";
-  return (
-    <article className="book">
-      <img src="./images/book-1.jpg" alt="Book Cover" height="250px" />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>Amazon Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
