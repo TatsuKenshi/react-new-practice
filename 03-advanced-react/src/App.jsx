db04d5ad38be1178c29a7a018762bb31bfef4ec7
TatsuKenshi <rockinbg@gmail.com>
2026-09-05 19:45:14 +0200

    112-117

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
 
 function App() {
   return (
--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,53 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [user, setUser] = useState(false);
+  console.log("render");
+
+  return (
+    <>
+      <div>
+        <h2>Cleanup Function Example</h2>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => {
+            setUser(!user);
+          }}
+        >
+          click me
+        </button>
+      </div>
+      {user && <SecondComponent />}
+    </>
+  );
+};
+
+const SecondComponent = () => {
+  useEffect(() => {
+    // console.log("initial render");
+    const intID = setInterval(() => {
+      // console.log("Hello from interval");
+    }, 1000);
+    return () => {
+      clearInterval(intID);
+      console.log("cleanup");
+    };
+  }, []);
+
+  useEffect(() => {
+    const someFunc = () => {
+      // some code
+    };
+    window.addEventListener("scroll", someFunc);
+    return () => window.removeEventListener("scroll", someFunc);
+  }, []);
+
+  return (
+    <div>
+      <h2>Second component</h2>
+    </div>
+  );
 };
 
 export default CleanupFunction;
